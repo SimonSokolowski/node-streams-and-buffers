2b36d442019d8b07711b29934c622eedfb7663b4
simon <simon.sokolowski@outlook.com>
2021-04-26 12:39:08 +0100

    Creating a read stream that shows text to website

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ var fs = require("fs");
 var myReadStream = fs.createReadStream(__dirname + "/readMe.txt");
 var myWriteStream = fs.createWriteStream(__dirname + "/writeMe.txt");
 
-myReadStream.on("data", function(chunk){
-    console.log("new chunk received:");
-    myWriteStream.write(chunk);
-});
+myReadStream.pipe(myWriteStream);
 
-/*
+
+// myReadStream.on("data", function(chunk){
+//     console.log("new chunk received:");
+//     myWriteStream.write(chunk);
+// });
+
+
 var server = http.createServer(function(req, res){
     console.log("request was made " + req.url);
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hey, this is a test.");
+    var myReadStream = fs.createReadStream(__dirname + "/readMe.txt", "utf8");
+    myReadStream.pipe(res);
 });
 
 server.listen(3000, "127.0.0.1");
 console.log("Now listening to port 3000");
-*/
